6970e29551d7356fdc0d78aa64b3a2d27a313576
Nattapon Pondongnok <nainatjab999@gmail.com>
2021-01-31 01:17:57 +0700

    set UNIQUE short_url column

--- sql-init.d/00-init.sql
+++ sql-init.d/00-init.sql
@@ -4,5 +4,7 @@ CREATE TABLE IF NOT EXISTS links_db.short_urls(
     id BIGSERIAL PRIMARY KEY,
     short_url VARCHAR(10) NOT NULL,
     full_url TEXT NOT NULL,
-    user_id integer NOT NULL DEFAULT '1'
+    user_id integer NOT NULL DEFAULT '1',
+    
+    UNIQUE(short_url)
 );
